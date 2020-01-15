60e605351a005290a44f069e67c380f5a9b71233
crushcat <crushcat@gmail.com>
2020-01-15 16:50:37 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length) {
+        return array.reduce((acc, val) => {
+            return acc < val ? acc : val;
+        });
+    }
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length) {
+        return array.reduce((acc, val) => {
+            return acc > val ? acc : val;
+        });
+    }
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length) {
+        return (
+            array.reduce((acc, val) => {
+                return acc + val;
+            }) / array.length
+        );
+    }
+    return 0;
+};
